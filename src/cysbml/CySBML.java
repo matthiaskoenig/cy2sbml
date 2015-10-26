591e3d2b0311ef51f09c56e6d345618bc97ffaf7
Matthias Koenig <konigmatt@googlemail.com>
2015-10-26 19:05:54 +0100

    Version number increased

--- src/cysbml/CySBML.java
+++ src/cysbml/CySBML.java
@@ -141,7 +141,7 @@ import cysbml.SBMLFilter;
  */
 public class CySBML extends CytoscapePlugin implements PropertyChangeListener{
 	public static final String NAME = "cy2sbml"; 
-	public static final String VERSION = "v1.4.0";
+	public static final String VERSION = "v1.4.1";
 	public static LogCyPlugin LOGGER = new LogCyPlugin(NAME);
 	
 	public static final boolean DEBUG = true;
